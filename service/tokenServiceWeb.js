a7f38d02939ebb66c147009155881dbe6f0c106f
mannysanz715 <mannysanz715@gmail.com>
2023-09-12 09:43:37 -0700

    Update

---
+++ service/tokenServiceWeb.js
@@ -0,0 +1,35 @@
+import jwt_decode from 'jwt-decode'
+import { Platform } from 'react-native'
+
+if(Platform.OS == 'web'){
+  function setToken(token) {
+    localStorage.setItem('token', token)
+  }
+  
+  function getToken() {
+    let token = localStorage.getItem('token')
+    if (token) {
+      const payload = jwt_decode(token)
+      if (payload.exp < Date.now() / 1000) {
+        localStorage.removeItem('token')
+        token = null
+      }
+    } else {
+      localStorage.removeItem('token')
+    }
+    return token
+  }
+  
+  function getUserFromToken() {
+    const token = getToken()
+    return token ? jwt_decode(token).user : null
+  }
+  
+  function removeToken() {
+    localStorage.removeItem('token')
+  }
+  
+}
+
+
+export { setToken, getToken, getUserFromToken, removeToken }
